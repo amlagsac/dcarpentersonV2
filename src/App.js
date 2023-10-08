b2d1be8d3798b859783a36f700df5c1b6af0bb82
omi-andrelagsac <andre.lagsac@smsupermalls.com>
2023-10-08 22:53:48 +0800

    Added React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Products from './components/Products/Products';
+import Services from './components/Services/Services';
+import About from './components/About/About';
+import ContactUs from './components/ContactUs/ContactUs';
+import NoPage from './components/NoPage/NoPage';
 import './App.css';
 
 function App() {
   return (
     <>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path="/products" element={<Products />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
     </>
   );
 }
